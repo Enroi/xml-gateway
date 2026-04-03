277a6d1cc0d9bbe3e2f1da0337a2c41800bb3601
Sergey Antonov <santonov@ibs.ru>
2026-04-03 15:46:42 +0300

    Добавил ResultController::results

---
+++ src/main/kotlin/ru/ibs/transform/xml/controllers/ResultController.kt
@@ -0,0 +1,38 @@
+package ru.ibs.transform.xml.controllers
+
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.Date
+import java.util.UUID
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/result")
+class ResultController {
+
+    data class SingleResult (
+        val id: Long,
+        val dateInput: LocalDateTime,
+        val dateOutput: LocalDateTime,
+    )
+
+    data class BatchResults (
+        val data: List<SingleResult>,
+        val page: Int,
+        val pages: Int,
+        val rows: Int,
+    )
+
+    @GetMapping("all")
+    fun results(): BatchResults {
+        return BatchResults(
+            listOf(
+                SingleResult(1, LocalDateTime.now(), LocalDateTime.now()),
+                SingleResult(2, LocalDateTime.now(), LocalDateTime.now()),
+            ), 0, 1, 10)
+    }
+}
